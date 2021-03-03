4584ef30e0e5cc0c44517bce1878d9f5bf829f77
akasharora506 <61285739+akasharora506@users.noreply.github.com>
2021-03-03 14:25:17 +0530

    Time: 20 ms (76.46%), Space: 17.9 MB (75.88%) - LeetHub

--- missing-number/missing-number.cpp
+++ missing-number/missing-number.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_set<int> s;
-        for(int i=0;i<nums.size();i++){
-            s.insert(i);
-        };
-        for(int i:nums)s.erase(i);
+        int n=nums.size();
+        int sumShouldBe= n*(n+1)/2;
+        int sumIs=0;
+        for(int i:nums) sumIs+=i;
+        return abs(sumIs-sumShouldBe);
         
-        return !s.empty()?*s.begin():nums.size();
     }
 };
