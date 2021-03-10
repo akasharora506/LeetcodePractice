6d14247c3476ca1d4fc1ecf163093d7359964ee4
akasharora506 <61285739+akasharora506@users.noreply.github.com>
2021-03-10 14:45:11 +0530

    Time: 76 ms (26.17%), Space: 22.4 MB (93.07%) - LeetHub

---
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -0,0 +1,41 @@
+class RandomizedSet {
+public:
+    /** Initialize your data structure here. */
+    unordered_set<int> s;
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if(s.find(val)!=s.end()){
+            return false;
+        };
+        s.insert(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if(s.find(val)==s.end())
+            return false;
+        s.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        auto i=s.begin();
+        int n=s.size();
+        advance(i,rand()%n);
+        return *i;
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
