8937db23c10c7b886874670a250155d19fb82ffb
akasharora506 <61285739+akasharora506@users.noreply.github.com>
2021-03-19 13:59:45 +0530

    Time: 12 ms (97.34%), Space: 9.5 MB (87.57%) - LeetHub

---
+++ number-of-islands/number-of-islands.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dfs(vector<vector<char>>& grid,int i,int j){
+        if(i<0||j<0||i>=grid.size()||j>=grid[0].size()||grid[i][j]=='0')
+            return 0;
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+        return 1;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        if(n==0)
+            return 0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    ans+=dfs(grid,i,j);
+                };
+            };
+        };
+        return ans;
+    }
+};
