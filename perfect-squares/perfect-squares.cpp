5dede73c52e03caee94fb3e35870910dd3b14773
akasharora506 <61285739+akasharora506@users.noreply.github.com>
2021-03-28 21:04:57 +0530

    Time: 168 ms (55.20%), Space: 11.5 MB (17.84%) - LeetHub

--- perfect-squares/perfect-squares.cpp
+++ perfect-squares/perfect-squares.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int> dp(n+1,INT_MAX);
+        vector<long long int> dp(n+1,INT_MAX);
         dp[0]=0;
-        int maxDig=sqrt(n);
-        for(int i=1;i<=maxDig;i++){
-            dp[i*i]=1;
-        };
+        dp[1]=1;
         for(int i=2;i<=n;i++){
-            for(int j=1;j<=maxDig&&i>=j*j;j++){
-              dp[i]=min(dp[i],dp[i-j*j]+1);  
+            if(floor(sqrt(i))==ceil(sqrt(i))){
+                dp[i]=1;
+                continue;
+            };
+            for(int j=1;j*j<=i;j++){
+                dp[i]=min(dp[i],1+dp[i-j*j]);
             };
         };
         return dp[n];
