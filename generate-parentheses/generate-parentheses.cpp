70dbfc0ff0eb8441fae8c65d39aa8abab9f4417f
akasharora506 <61285739+akasharora506@users.noreply.github.com>
2021-06-16 14:39:29 +0530

    Time: 4 ms (75.55%), Space: 15 MB (30.08%) - LeetHub

--- generate-parentheses/generate-parentheses.cpp
+++ generate-parentheses/generate-parentheses.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    bool isValid(string a){
-        stack<char> s;
-        for(char i:a){
-            if(i=='('){
-                s.push('(');
-            }else{
-                if(s.empty())
-                    return 0;
-                s.pop();
-            };
-        };
-        return s.empty();
-    };
-    void backtrack(vector<string> &ans,string curr,int open,int close,int max){
-        if(open==max&&close==max){
-            if(isValid(curr))ans.push_back(curr);
+    void help(vector<string> &ans,int n,int o=0,int c=0,string curr=""){
+        if(c>o||c>n||o>n)return;
+        if(c==n&&o==n){
+            ans.push_back(curr);
             return;
         };
-        if(open!=max&&close!=max){
-            backtrack(ans,curr+'(',open+1,close,max);
-            backtrack(ans,curr+')',open,close+1,max);
-        }else if(open==max){
-            backtrack(ans,curr+')',open,close+1,max);
-        }else{
-            backtrack(ans,curr+'(',open+1,close,max);
-        };
-    }
+        help(ans,n,o+1,c,curr+"(");
+        help(ans,n,o,c+1,curr+")");
+    };
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        backtrack(ans,"",0,0,n);
+       vector<string> ans;
+        help(ans,n);
         return ans;
     }
 };
