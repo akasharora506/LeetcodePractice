97bfe5857a08c43d5734b9ae27b440f0001c5cb8
akasharora506 <61285739+akasharora506@users.noreply.github.com>
2021-07-01 13:59:40 +0530

    Time: 528 ms (6.66%), Space: 14.1 MB (41.92%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -13,7 +13,18 @@ public:
         int sum=0,n=nums.size();
         for(int i:nums)sum+=i;
         if(sum%2)return false;
-        vector<vector<int>> dp(n+1,vector<int> (sum+1,-1));
-        return help(dp,nums,n,sum/2);
+        vector<vector<bool>> dp(n+1,vector<bool> (sum+1,0));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(i==0){
+                    dp[i][j]=(j==0);
+                }else if(j>=nums[i-1]){
+                    dp[i][j]=dp[i-1][j]||dp[i-1][j-nums[i-1]];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                };
+            };
+        };
+        return dp[n][sum/2];
     }
 };
