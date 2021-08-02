974ea302861398931b9f98f8d32795c3d889bd4d
akasharora506 <61285739+akasharora506@users.noreply.github.com>
2021-08-02 13:29:18 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (43.03%) - LeetHub

--- sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -11,23 +11,48 @@
  */
 class Solution {
 public:
-    void help(TreeNode* root,unordered_multiset<string> &v,string s=""){
-        if(!root)
-            return;
-        if(!root->left&&!root->right){
-            v.insert(s+to_string(root->val));
-            return;
-        };
-        help(root->left,v,s+to_string(root->val));
-        help(root->right,v,s+to_string(root->val));
-    }
+    // void help(TreeNode* root,unordered_multiset<string> &v,string s=""){
+    //     if(!root)
+    //         return;
+    //     if(!root->left&&!root->right){
+    //         v.insert(s+to_string(root->val));
+    //         return;
+    //     };
+    //     help(root->left,v,s+to_string(root->val));
+    //     help(root->right,v,s+to_string(root->val));
+    // }
+    // int sumNumbers(TreeNode* root) {
+    //     unordered_multiset<string> v;
+    //     help(root,v);
+    //     int ans=0;
+    //     for(auto i:v){
+    //         ans+=stoi(i);
+    //     };
+    //     return ans;
+    // }
     int sumNumbers(TreeNode* root) {
-        unordered_multiset<string> v;
-        help(root,v);
-        int ans=0;
-        for(auto i:v){
-            ans+=stoi(i);
-        };
-        return ans;
-    }
+		if (root == NULL)
+			return 0;
+		int sum = 0;
+		auto trie = unordered_map<const TreeNode*, int>();
+		auto dfs = vector<TreeNode*>(1, root);
+		while (dfs.size()) {
+			const TreeNode * n = dfs.back();
+			dfs.pop_back();
+			const int val = trie[n] * 10 + n->val;
+			if (n->left == NULL && n->right == NULL)
+				sum += val;	// Add val into sum if it's a leave
+			else {
+				if (n->left) {
+					trie[n->left] = val;	// Save the val (from root to parent)
+					dfs.emplace_back(n->left);
+				}
+				if (n->right) {
+					trie[n->right] = val;
+					dfs.emplace_back(n->right);
+				}
+			}
+		}
+		return sum;
+	}
 };
