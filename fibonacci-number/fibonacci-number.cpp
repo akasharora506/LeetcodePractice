ec15db414b5dc41e7471f7bced4e821d9353e2c2
akasharora506 <61285739+akasharora506@users.noreply.github.com>
2021-10-04 22:15:36 +0530

    Time: 30 ms (5.35%), Space: 5.8 MB (79.63%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int fib(int n) {
-        // vector<int> dp(n+1);
-        // dp[0]=0;
-        // for(int i=1;i<=n;i++){
-        //     if(i==1){
-        //         dp[i]=1;
-        //         continue;
-        //     };
-        //     dp[i]=dp[i-1]+dp[i-2];
-        // };
-        // return dp[n];
+        // base case
         if(n==0||n==1)return n;
-        return fib(n-1)+fib(n-2);
+        // recall
+        int a=fib(n-1),b=fib(n-2);
+        // content
+        return a+b;
     }
 };
